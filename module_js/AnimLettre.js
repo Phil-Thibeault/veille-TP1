9b2794038e08eae8bea938499839b712c4965128
Phil <philippe.thibeault99@outlook.com>
2019-02-12 10:52:14 -0500

    initialization

---
+++ module_js/AnimLettre.js
@@ -0,0 +1,61 @@
+import {
+    Util
+} from './Util.js'
+
+
+export class AnimLettre {
+
+    /**
+     * Classe permettant de créer et d'animer une introduction
+     * @param {string} lesLettres - contient l'ensemble des mots d'intro
+     * @param {DOMElement} elementParent - Conteneur de l'animation
+     * @param {function} fonction - l'adresse de la fonction à exécuter après l'animation
+         
+     }}
+     */
+
+    constructor(lesLettres, elementParent, fonction) {
+        //Récupérer les valeurs passées en paramètre			
+        this.lesLettres = lesLettres;
+        this.elmParent = elementParent
+        this.animLettres(this.lesLettres)
+        this.fonction = fonction
+    }
+
+
+    animLettres(lesLettres) {
+        /* Création des élément DOM qui seront animés. 
+        Les éléments seront intégré dans le conteneur elmParent
+        */
+        console.log('mot')
+        let elmConteneur = this.creerElement(this.elmParent,
+            'section',
+            '',
+            'mot')
+        /* On garde une référence sur la fonction terminerIntro */
+        let refTerminerIntro = this.terminerAnim.bind(this)
+        elmBouton.addEventListener('mousedown', this.terminerIntro.bind(this))
+    }
+
+    creerElement(elmParent, balise, contenu, classCSS) {
+        console.log(balise)
+        let noeud = document.createElement(balise)
+        if (contenu != '') {
+            noeud.innerHTML = contenu
+        }
+        noeud.classList.add(classCSS)
+        elmParent.appendChild(noeud)
+        return noeud
+    }
+
+    terminerAnim(evt) {
+        this.elmParent.firstChild.classList.add('deplacementContenuIntro')
+        this.elmParent.firstChild.addEventListener('animationend', this.passerVersAnimationSuivante.bind(this))
+    }
+
+    passerVersAnimationSuivante(evt) {
+        Util.detruireTousLesNoeud(this.elmParent, this.elmParent)
+        this.fonction()
+    }
+
+}
