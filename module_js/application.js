cd512e2cdd2b44991935efa18542d90c729a1764
Phil <philippe.thibeault99@outlook.com>
2019-02-21 18:38:37 -0500

    positionnement nouvelles sections

--- module_js/application.js
+++ module_js/application.js
@@ -1,25 +1,21 @@
-
-import {contenuIntro} from './contenuIntro.js' // le contenu de l'intoduction */
-import {Introduction} from './Introduction.js' // 
-import {AnimLettre} from './AnimLettre.js' //
-
+import { contenuIntro } from "./contenuIntro.js"; // le contenu de l'intoduction */
+import { Introduction } from "./Introduction.js"; //
+import { AnimLettre } from "./AnimLettre.js"; //
 
 /* l'élement de la page qui contiendra les éléments créés dynamiquement */
-let elmHeader = document.querySelector('.header')
-let intro = new Introduction(contenuIntro, elmHeader, animationLettre)
+let elmHeader = document.querySelector(".header");
+let intro = new Introduction(contenuIntro, elmHeader, animationLettre);
 
 // debutQuestionnaire()
 
-function animationLettre()
-{
-    /* Une fois que l'animation des mots est terminé la fonction animLettre s'exécutera */ 
-	console.log('debut animation lettre')
-	const lesLettres = 'Veille-technologique'
- 	let  monAnimLettre = new AnimLettre(lesLettres, elmHeader, finAnim)	
+function animationLettre() {
+  /* Une fois que l'animation des mots est terminé la fonction animLettre s'exécutera */
+
+  console.log("debut animation lettre");
+  const lesLettres = "Seigneur des Anneaux";
+  let monAnimLettre = new AnimLettre(lesLettres, elmHeader, finAnim);
 }
 
-
-function finAnim()
-{
-	console.log('animation terminée')
+function finAnim() {
+  console.log("animation terminée");
 }
